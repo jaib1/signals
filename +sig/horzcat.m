bfb4acf8cf20e66e77917307297305fab2e087a0
k1o0 <k1o0@3tk.co>
2018-10-18 19:12:29 +0100

    Docstring for horz- and vertcat

--- +sig/horzcat.m
+++ +sig/horzcat.m
@@ -1,6 +1,19 @@
 function C = horzcat(varargin)
-%UNTITLED7 Summary of this function goes here
-%   Detailed explanation goes here
+%HORZCAT Horizontally concatenate multiple inputs
+%   Returns signal what contains multiple values concatenated.  Inputs may
+%   be a mix of signals and non-signals.
+%
+%   Examples:
+%     net = sig.Net;
+%     sig1 = net.origin('1');
+%     sig2 = net.origin('2');
+%     inp3 = pi;
+%     C = sig.horzcat(sig1,sig2,inp3)
+%     sig1.post(1.0)
+%     sig2.post(2.0)
+%     >> 1.0000    2.0000    3.1416
+%
+% See also HORZCAT, SIG.VERTCAT
 
 b = cellfun(@(a)isa(a, 'sig.Signal'), varargin);
 if ~any(b)
--- +sig/vertcat.m
+++ +sig/vertcat.m
@@ -1,6 +1,21 @@
 function C = vertcat(varargin)
-%UNTITLED7 Summary of this function goes here
-%   Detailed explanation goes here
+%VERTCAT Vertically concatenate multiple inputs
+%   Returns signal what contains multiple values concatenated.  Inputs may
+%   be a mix of signals and non-signals.
+%
+%   Examples:
+%     net = sig.Net;
+%     sig1 = net.origin('1');
+%     sig2 = net.origin('2');
+%     inp3 = pi;
+%     C = sig.vertcat(sig1,sig2,inp3)
+%     sig1.post(1.0)
+%     sig2.post(2.0)
+%     >> 1.0000
+%        2.0000
+%        3.1416
+%
+% See also VERTCAT, SIG.HORZCAT
 
 b = cellfun(@(a)isa(a, 'sig.Signal'), varargin);
 if ~any(b)
